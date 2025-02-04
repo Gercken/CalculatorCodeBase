c300271ab48a696d9c32cbef3aeaaec18b1f29ca
Alexander Gerckens <112266323+Gercken@users.noreply.github.com>
2025-02-04 13:29:19 +0100

    MORE TESTS

--- Calculator/obj/Debug/net8.0/Calculator.AssemblyInfo.cs
+++ Calculator/obj/Debug/net8.0/Calculator.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Calculator")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+af2e363b04ab4a163ffe32d307fb23d8ca3474e9")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a9d868d76d07620c546fc41c256b3a7b35b2aa32")]
 [assembly: System.Reflection.AssemblyProductAttribute("Calculator")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Calculator")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
